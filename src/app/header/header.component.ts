3bff788304e341aeee4de79cbc6f42fbd06a258a
samuel@keycash.io <samuel@keycash.io>
2020-09-09 18:20:50 -0300

    Header atualizado

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  isCollapsed:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCollapse () {
+    this.isCollapsed = !this.isCollapsed
   }
 
 }
